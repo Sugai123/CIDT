3d18a8430bb0e118c4221cf1b3e6862867c357b3
Sugai123 <1003279959@qq.com>
2022-04-01 21:16:09 +0800

    week6
    javascrpit

---
+++ script.js
@@ -0,0 +1,19 @@
+document.write("Hello world!");
+
+function sayHello() {
+  var response = prompt("What's your name? QAQ");
+  alert("Hello " + response + ", have a nice day! XD");
+}
+
+function showTopic() {
+  var x = document.getElementById('demo');
+  x.style.fontSize = "25px";
+  x.style.color = "red";
+}
+
+
+function add(a,b){
+  return a + b;
+}
+console.log(add(2000,2));
+console.log(add(999,112));
